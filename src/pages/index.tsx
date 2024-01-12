75c7a55d0bcf84583e2519597d9bec5c6a9be753
ashtonHolgate <ashton.holgate@sainsburys.co.uk>
2024-01-12 10:51:27 +0000

    Added pixi logo to pixi render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
 const PixiWrapper = dynamic(() => import("../components/PixiWrapper"), {
   ssr: false,
-}); 
+});
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <PixiWrapper />
+    <div className="width-max height-max relative">
+      <Image src="https://pixijs.com/images/logo.svg" alt="pixi logo" className="absolute top-10 right-10" width={100} height={50}/>
+      <PixiWrapper />
+    </div>
   );
 }
